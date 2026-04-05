4bf450e6597ab56561c5e5442576229503ac18ce
Mikhail <kolhan.mihail@gmail.com>
2026-04-05 20:18:18 +0200

    add bases

--- data/menu.py
+++ data/menu.py
@@ -1,4 +1,49 @@
 #tutaj sie znajduje menu naszego programu
-
-def wybor_opcji_menu
-    ...
+import sys
+#funkcja wyswitlajaca pomoc
+def show_help():
+    print("Help         Show this message")
+    print("Print        Print the tree usin In-order, Pre-order, Post-order")
+    print("Remove       Remove elements of the tree")
+    print("Delete       Delete the whole tree")
+    print("Export       Export tree to tikzpicture")
+    print("Rebalance    Rebalance tree")
+    print("Exit         Exit the program(same as Ctrl+D)")
+#glowna fukcja
+def main():
+    try:
+        while True:
+        
+            try:
+                command =input("action> ").strip()
+            except EOFError:
+                break
+            if command == "Help":
+                show_help()
+            elif command == "Print":
+                print("----Wypisywanie drzewa w trzech postaciach----")
+                #tu jeszcze nie zrobilismy(wyswietla pre, in, post -order)
+            elif command == "Remove":
+                print("----Usuwanie elementu/ow drzewa----")
+                #func to delete tree
+            elif command == "Delete":
+                print("----Usuwanie calkowite drzewa----")
+                #func to delete whole tree
+            elif command == "Export":
+                print("----Eksport drzewa do tikzpicture----")
+                #func to export tree to tikzpicture
+            elif command == "Rebalance":
+                print("----Przestawianie drzewa----")
+                #func to rebalance tree
+            elif command == "Exit":
+                break
+            else:
+                print(f"Unknown command: {command}")
+    except (KeyboardInterrupt, EOFError):
+            pass
+    except ValueError:
+            sys.exit(1)
+    print("Programm exited with status 0")
+if __name__ == "__main__":
+    main()
+        
