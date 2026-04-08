17c6f00ff9e32c89adbc27e5e9e8b0b34de3a52b
Anastasiya Haiduk <anastasiagajduk24@gmail.com>
2026-04-08 12:39:45 +0200

    changes again in remove func

--- data/remove_function.py
+++ data/remove_function.py
@@ -2,22 +2,29 @@ def handle_remove(tree):
     if tree.root is None:
         print("Tree is empty")
         return
+
     try:
-        val_str = input("value/s to remove> ").strip()
-        
+        nodes_str = input("nodes> ").strip()
+        if not nodes_str:
+            return
+        nodes_count = int(nodes_str)
+    
+        val_str = input("delete> ").strip()
         if not val_str:
             return
         
         elements = [int(x) for x in val_str.split()]
-       
+        
+        elements = elements[:nodes_count]
+        
         for val in elements:
             if tree.search(val):
                 tree.remove(val)
                 print(f"Value {val} was removed from the tree.")
             else:
                 print(f"Value {val} is not in the tree.")
-        
+                
     except ValueError:
-        print("Invalid input. Please enter numbers separated by spaces.")
+        print("Invalid input. Please enter numbers.")
     except EOFError:
         pass
